55b2b3f364522ac03e558f3eba2e1b099180249d
Alexander <62954809+AlexanderZhmurkin@users.noreply.github.com>
2025-03-04 19:15:27 +0500

    Initial AdvancedUtility

---
+++ Source/AdvancedPlugin/Private/AdvancedUtility.cpp
@@ -0,0 +1,160 @@
+// Copyright AlexanderAL123. All Rights Reserved.
+
+#include "AdvancedUtility.h"
+#include "Chaos/Particle/ParticleUtilities.h"
+#include "Chaos/Utilities.h"
+#include "PhysicsProxy/SingleParticlePhysicsProxy.h"
+
+void FVehicleStatus::CaptureState(const FBodyInstance* TargetInstance)
+{
+	if (!TargetInstance) return;
+
+	VehicleWorldTransform = TargetInstance->GetUnrealWorldTransform();
+	VehicleWorldVelocity = TargetInstance->GetUnrealWorldVelocity();
+	VehicleWorldVelocityNormal = VehicleWorldVelocity.GetSafeNormal();
+	VehicleWorldAngularVelocity = TargetInstance->GetUnrealWorldAngularVelocityInRadians();
+	VehicleWorldCOM = TargetInstance->GetCOMPosition();
+
+	VehicleXVector = VehicleWorldTransform.GetUnitAxis(EAxis::X);
+	VehicleYVector = VehicleWorldTransform.GetUnitAxis(EAxis::Y);
+	VehicleZVector = VehicleWorldTransform.GetUnitAxis(EAxis::Z);
+
+	VehicleForwardSpeed = FVector::DotProduct(VehicleWorldVelocity, VehicleXVector);
+	VehicleRightSpeed = FVector::DotProduct(VehicleWorldVelocity, VehicleYVector);
+}
+
+void FVehicleStatus::CaptureState(const Chaos::FRigidBodyHandle_Internal* TargetHandle)
+{
+	if (!TargetHandle) return;
+
+	const FTransform WorldTM(TargetHandle->R(), TargetHandle->X());
+	VehicleWorldTransform = WorldTM;
+	VehicleWorldVelocity = TargetHandle->V();
+	VehicleWorldVelocityNormal = VehicleWorldVelocity.GetSafeNormal();
+	VehicleWorldAngularVelocity = TargetHandle->W();
+	VehicleWorldCOM = TargetHandle->CenterOfMass();
+
+	VehicleXVector = VehicleWorldTransform.GetUnitAxis(EAxis::X);
+	VehicleYVector = VehicleWorldTransform.GetUnitAxis(EAxis::Y);
+	VehicleZVector = VehicleWorldTransform.GetUnitAxis(EAxis::Z);
+
+	VehicleForwardSpeed = FVector::DotProduct(VehicleWorldVelocity, VehicleXVector);
+	VehicleRightSpeed = FVector::DotProduct(VehicleWorldVelocity, VehicleYVector);
+}
+
+void FVehicleForces::AddForce(Chaos::FRigidBodyHandle_Internal* RigidHandle, const FRacingVehicleApplyForce_DATA& DataIn)
+{
+	if (ensure(RigidHandle))
+	{
+		Chaos::EObjectStateType ObjectState = RigidHandle->ObjectState();
+		if (CHAOS_ENSURE(ObjectState == Chaos::EObjectStateType::Dynamic || ObjectState == Chaos::EObjectStateType::Sleeping))
+		{
+			if ((DataIn.Flags & EForcesFlags::AccelChange) == EForcesFlags::AccelChange)
+			{
+				const float RigidMass = RigidHandle->M();
+				const Chaos::FVec3 Acceleration = DataIn.Force * RigidMass;
+				RigidHandle->AddForce(Acceleration, false);
+			}
+			else
+			{
+				RigidHandle->AddForce(DataIn.Force, false);
+			}
+		}
+	}
+}
+
+void FVehicleForces::AddForceAtPosition(Chaos::FRigidBodyHandle_Internal* RigidHandle, const FRacingVehicleApplyForceAtPosition_DATA& DataIn)
+{
+	if (ensure(RigidHandle))
+	{
+		const Chaos::FVec3 WorldCOM = Chaos::FParticleUtilitiesGT::GetCoMWorldPosition(RigidHandle);
+		const Chaos::FVec3 WorldTorque = Chaos::FVec3::CrossProduct(DataIn.Position - WorldCOM, DataIn.Force);
+		RigidHandle->AddForce(DataIn.Force, false);
+		RigidHandle->AddTorque(WorldTorque, false);
+	}
+}
+
+void FVehicleForces::AddTorque(Chaos::FRigidBodyHandle_Internal* RigidHandle, const FRacingVehicleAddTorqueInRadians_DATA& DataIn)
+{
+	if (ensure(RigidHandle))
+	{
+		Chaos::EObjectStateType ObjectState = RigidHandle->ObjectState();
+
+		if (CHAOS_ENSURE(ObjectState == Chaos::EObjectStateType::Dynamic || ObjectState == Chaos::EObjectStateType::Sleeping))
+		{
+			if ((DataIn.Flags & EForcesFlags::AccelChange) == EForcesFlags::AccelChange)
+			{
+				RigidHandle->AddTorque(Chaos::FParticleUtilitiesXR::GetWorldInertia(RigidHandle) * DataIn.Torque, false);
+			}
+			else
+			{
+				RigidHandle->AddTorque(DataIn.Torque, false);
+			}
+		}
+	}
+}
+
+void FVehicleForces::AddImpulse(Chaos::FRigidBodyHandle_Internal* RigidHandle, const FRacingVehicleAddImpulse_DATA& DataIn)
+{
+	if (ensure(RigidHandle))
+	{
+		if ((DataIn.Flags & EForcesFlags::VelChange) == EForcesFlags::VelChange)
+		{
+			RigidHandle->SetLinearImpulse(RigidHandle->LinearImpulse() + RigidHandle->M() * DataIn.Impulse, false);
+		}
+		else
+		{
+			RigidHandle->SetLinearImpulse(RigidHandle->LinearImpulse() + DataIn.Impulse, false);
+		}
+	}
+}
+
+void FVehicleForces::AddImpulseAtPosition(Chaos::FRigidBodyHandle_Internal* RigidHandle, const FRacingVehicleAddImpulseAtPosition_DATA& DataIn)
+{
+	if (ensure(RigidHandle))
+	{
+		const Chaos::FVec3 WorldCOM = Chaos::FParticleUtilitiesGT::GetCoMWorldPosition(RigidHandle);
+		const Chaos::FVec3 AngularImpulse = Chaos::FVec3::CrossProduct(DataIn.Position - WorldCOM, DataIn.Impulse);
+		RigidHandle->SetLinearImpulse(RigidHandle->LinearImpulse() + DataIn.Impulse, false);
+		RigidHandle->SetAngularImpulse(RigidHandle->AngularImpulse() + AngularImpulse, false);
+	}
+}
+
+FVector FVehicleForces::GetVelocityAtPoint(const FBodyInstance* Body, const FVector& InPoint)
+{
+	if (FPhysicsActorHandle ActorHandle = Body->ActorHandle)
+	{
+		if (!IsInGameThread())
+		{
+			if (Chaos::FRigidBodyHandle_Internal* RigidHandle = ActorHandle->GetPhysicsThreadAPI())
+			{
+				if (ensure(RigidHandle))
+				{
+					const Chaos::FVec3 COM = RigidHandle->X() + RigidHandle->R() * RigidHandle->CenterOfMass();
+					const Chaos::FVec3 Diff = InPoint - COM;
+					return RigidHandle->V() - Chaos::FVec3::CrossProduct(Diff, RigidHandle->W());
+				}
+			}
+			return FVector::ZeroVector;
+		}
+		else
+		{
+			Chaos::FRigidBodyHandle_External& RigidHandle = ActorHandle->GetGameThreadAPI();
+			const Chaos::FVec3 COM = RigidHandle.X() + RigidHandle.R() * RigidHandle.CenterOfMass();
+			const Chaos::FVec3 Diff = InPoint - COM;
+			return RigidHandle.V() - Chaos::FVec3::CrossProduct(Diff, RigidHandle.W());
+		}
+	}
+	return FVector::ZeroVector;
+}
+
+FVector FVehicleForces::GetVelocityAtPoint(Chaos::FRigidBodyHandle_Internal* RigidHandle, const FVector& PointIn)
+{
+	if (ensure(RigidHandle))
+	{
+		const Chaos::FVec3 COM = RigidHandle->X() + RigidHandle->R() * RigidHandle->CenterOfMass();
+		const Chaos::FVec3 Diff = PointIn - COM;
+		return RigidHandle->V() - Chaos::FVec3::CrossProduct(Diff, RigidHandle->W());
+	}
+	return FVector::ZeroVector;
+}
---
+++ Source/AdvancedPlugin/Public/AdvancedUtility.h
@@ -0,0 +1,196 @@
+// Copyright AlexanderAL123. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+enum class EForcesFlags : uint32
+{
+	None = 0,
+	AllowSubstepping = 1 << 0,
+	AccelChange = 1 << 1,
+	VelChange = 1 << 2,
+	IsLocalForce = 1 << 3
+};
+ENUM_CLASS_FLAGS(EForcesFlags);
+
+struct ADVANCEDPLUGIN_API FVehicleStatus
+{
+public:
+	FVehicleStatus()
+		: VehicleWorldTransform(FTransform::Identity)
+		, VehicleWorldVelocity(FVector::ZeroVector)
+		, VehicleWorldVelocityNormal(FVector::ZeroVector)
+		, VehicleWorldAngularVelocity(FVector::ZeroVector)
+		, VehicleWorldCOM(FVector::ZeroVector)
+		, VehicleLocalVelocity(FVector::ZeroVector)
+		, VehicleXVector(FVector::ZeroVector)
+		, VehicleYVector(FVector::ZeroVector)
+		, VehicleZVector(FVector::ZeroVector)
+		, VehicleForwardSpeed(0.0f)
+		, VehicleRightSpeed(0.0f)
+	{
+	}
+
+	/** Body Instance cache capture state */
+	void CaptureState(const FBodyInstance* TargetInstance);
+	/** Particle Handle cache capture state */
+	void CaptureState(const Chaos::FRigidBodyHandle_Internal* TargetHandle);
+
+	FTransform VehicleWorldTransform;
+	FVector VehicleWorldVelocity;
+	FVector VehicleWorldVelocityNormal;
+	FVector VehicleWorldAngularVelocity;
+	FVector VehicleWorldCOM;
+
+	FVector VehicleLocalVelocity;
+
+	FVector VehicleXVector;   //X Vector
+	FVector VehicleYVector;   //Y Vector
+	FVector VehicleZVector;   //Z Vector
+
+	float VehicleForwardSpeed;
+	float VehicleRightSpeed;
+};
+
+class ADVANCEDPLUGIN_API FVehicleForces
+{
+public:
+	struct FRacingVehicleApplyForce_DATA
+	{
+		FRacingVehicleApplyForce_DATA(const FVector& ForceIn, bool bAllowSubsteppingIn, bool bAccelChangeIn)
+			: Force(ForceIn), Flags(EForcesFlags::None)
+		{
+			Flags |= bAllowSubsteppingIn ? EForcesFlags::AllowSubstepping : EForcesFlags::None;
+			Flags |= bAccelChangeIn ? EForcesFlags::AccelChange : EForcesFlags::None;
+		}
+
+		FVector Force;
+		EForcesFlags Flags;
+	};
+
+	struct FRacingVehicleApplyForceAtPosition_DATA
+	{
+		FRacingVehicleApplyForceAtPosition_DATA(const FVector& ForceIn, const FVector& PositionIn, bool bAllowSubsteppingIn, bool bIsLocalForceIn)
+			: Force(ForceIn), Position(PositionIn), Flags(EForcesFlags::None)
+		{
+			Flags |= bAllowSubsteppingIn ? EForcesFlags::AllowSubstepping : EForcesFlags::None;
+			Flags |= bIsLocalForceIn ? EForcesFlags::IsLocalForce : EForcesFlags::None;
+		}
+
+		FVector Force;
+		FVector Position;
+		EForcesFlags Flags;
+	};
+
+	struct FRacingVehicleAddTorqueInRadians_DATA
+	{
+		FRacingVehicleAddTorqueInRadians_DATA(const FVector& TorqueIn, bool bAllowSubsteppingIn, bool bAccelChangeIn)
+			: Torque(TorqueIn), Flags(EForcesFlags::None)
+		{
+			Flags |= bAllowSubsteppingIn ? EForcesFlags::AllowSubstepping : EForcesFlags::None;
+			Flags |= bAccelChangeIn ? EForcesFlags::AccelChange : EForcesFlags::None;
+		}
+
+		FVector Torque;
+		EForcesFlags Flags;
+	};
+
+	struct FRacingVehicleAddImpulse_DATA
+	{
+		FRacingVehicleAddImpulse_DATA(const FVector& ImpulseIn, const bool bVelChangeIn)
+			: Impulse(ImpulseIn), Flags(EForcesFlags::None)
+		{
+			Flags |= bVelChangeIn ? EForcesFlags::VelChange : EForcesFlags::None;
+		}
+
+		FVector Impulse;
+		EForcesFlags Flags;
+	};
+
+	struct FRacingVehicleAddImpulseAtPosition_DATA
+	{
+		FRacingVehicleAddImpulseAtPosition_DATA(const FVector& ImpulseIn, const FVector& PositionIn)
+			: Impulse(ImpulseIn), Position(PositionIn)
+		{
+		}
+
+		FVector Impulse;
+		FVector Position;
+	};
+
+	void Add(const FRacingVehicleApplyForce_DATA& ApplyForceDataIn)
+	{
+		ApplyForceDatas.Add(ApplyForceDataIn);
+	}
+
+	void Add(const FRacingVehicleApplyForceAtPosition_DATA& ApplyForceAtPositionDataIn)
+	{
+		ApplyForceAtPositionDatas.Add(ApplyForceAtPositionDataIn);
+	}
+
+	void Add(const FRacingVehicleAddTorqueInRadians_DATA& ApplyTorqueDataIn)
+	{
+		ApplyTorqueDatas.Add(ApplyTorqueDataIn);
+	}
+
+	void Add(const FRacingVehicleAddImpulse_DATA& ApplyImpulseDataIn)
+	{
+		ApplyImpulseDatas.Add(ApplyImpulseDataIn);
+	}
+
+	void Add(const FRacingVehicleAddImpulseAtPosition_DATA& ApplyImpulseAtPositionDataIn)
+	{
+		ApplyImpulseAtPositionDatas.Add(ApplyImpulseAtPositionDataIn);
+	}
+
+	void Apply(Chaos::FRigidBodyHandle_Internal* RigidHandle)
+	{
+		for (const FRacingVehicleApplyForce_DATA& Data : ApplyForceDatas)
+		{
+			AddForce(RigidHandle, Data);
+		}
+
+		for (const FRacingVehicleApplyForceAtPosition_DATA& Data : ApplyForceAtPositionDatas)
+		{
+			AddForceAtPosition(RigidHandle, Data);
+		}
+
+		for (const FRacingVehicleAddTorqueInRadians_DATA& Data : ApplyTorqueDatas)
+		{
+			AddTorque(RigidHandle, Data);
+		}
+
+		for (const FRacingVehicleAddImpulse_DATA& Data : ApplyImpulseDatas)
+		{
+			AddImpulse(RigidHandle, Data);
+		}
+
+		for (const FRacingVehicleAddImpulseAtPosition_DATA& Data : ApplyImpulseAtPositionDatas)
+		{
+			AddImpulseAtPosition(RigidHandle, Data);
+		}
+
+		ApplyForceDatas.Empty();
+		ApplyForceAtPositionDatas.Empty();
+		ApplyTorqueDatas.Empty();
+		ApplyImpulseDatas.Empty();
+		ApplyImpulseAtPositionDatas.Empty();
+	}
+
+	static FVector GetVelocityAtPoint(const FBodyInstance* Body, const FVector& PointIn);
+	static FVector GetVelocityAtPoint(Chaos::FRigidBodyHandle_Internal* RigidHandle, const FVector& PointIn);
+
+private:
+	void AddForce(Chaos::FRigidBodyHandle_Internal* RigidHandle, const FRacingVehicleApplyForce_DATA& DataIn);
+	void AddForceAtPosition(Chaos::FRigidBodyHandle_Internal* RigidHandle, const FRacingVehicleApplyForceAtPosition_DATA& DataIn);
+	void AddTorque(Chaos::FRigidBodyHandle_Internal* RigidHandle, const FRacingVehicleAddTorqueInRadians_DATA& DataIn);
+	void AddImpulse(Chaos::FRigidBodyHandle_Internal* RigidHandle, const FRacingVehicleAddImpulse_DATA& DataIn);
+	void AddImpulseAtPosition(Chaos::FRigidBodyHandle_Internal* RigidHandle, const FRacingVehicleAddImpulseAtPosition_DATA& DataIn);
+
+	TArray<FRacingVehicleApplyForce_DATA> ApplyForceDatas;
+	TArray<FRacingVehicleApplyForceAtPosition_DATA> ApplyForceAtPositionDatas;
+	TArray<FRacingVehicleAddTorqueInRadians_DATA> ApplyTorqueDatas;
+	TArray<FRacingVehicleAddImpulse_DATA> ApplyImpulseDatas;
+	TArray<FRacingVehicleAddImpulseAtPosition_DATA> ApplyImpulseAtPositionDatas;
+};
